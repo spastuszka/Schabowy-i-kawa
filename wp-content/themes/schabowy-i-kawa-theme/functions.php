6e04bc6fabbabb48966bbcf4003fe3e3ad0abfe6
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-07 19:25:15 +0100

    Added in function.php loaded lato font from google fonts

--- wp-content/themes/schabowy-i-kawa-theme/functions.php
+++ wp-content/themes/schabowy-i-kawa-theme/functions.php
@@ -2,6 +2,7 @@
 
 function pork_coffee_files()
 {
+  wp_enqueue_style('font-lato', '//fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('pork_coffee_main_styles', get_theme_file_uri('./build/style-index.css'));
   wp_enqueue_style('pork_coffee_normalize_styles', get_theme_file_uri('./build/index.css'));
