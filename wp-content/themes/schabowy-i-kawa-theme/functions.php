a44e538dd0bba73fd3f414c1c2d9fe9b557b3aad
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-07 19:16:22 +0100

    Change path default css path

--- wp-content/themes/schabowy-i-kawa-theme/functions.php
+++ wp-content/themes/schabowy-i-kawa-theme/functions.php
@@ -2,7 +2,7 @@
 
 function pork_coffee_files()
 {
-  wp_enqueue_style('pork_coffee_main_styles', get_stylesheet_uri());
+  wp_enqueue_style('pork_coffee_main_styles', get_theme_file_uri('./build/style-index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'pork_coffee_files');
