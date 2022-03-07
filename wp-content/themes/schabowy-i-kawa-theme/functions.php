1168bad43e9a087d9686be7477cdb9b4ec941409
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-07 19:17:53 +0100

    Added second css file to normalize website

--- wp-content/themes/schabowy-i-kawa-theme/functions.php
+++ wp-content/themes/schabowy-i-kawa-theme/functions.php
@@ -3,6 +3,7 @@
 function pork_coffee_files()
 {
   wp_enqueue_style('pork_coffee_main_styles', get_theme_file_uri('./build/style-index.css'));
+  wp_enqueue_style('pork_coffee_normalize_styles', get_theme_file_uri('./build/index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'pork_coffee_files');
