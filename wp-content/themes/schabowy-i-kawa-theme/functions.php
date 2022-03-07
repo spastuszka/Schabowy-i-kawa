37625adeb4490089266e03f76135826c85c38280
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-07 19:22:50 +0100

    Added css font awesome to load in website

--- wp-content/themes/schabowy-i-kawa-theme/functions.php
+++ wp-content/themes/schabowy-i-kawa-theme/functions.php
@@ -2,6 +2,7 @@
 
 function pork_coffee_files()
 {
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('pork_coffee_main_styles', get_theme_file_uri('./build/style-index.css'));
   wp_enqueue_style('pork_coffee_normalize_styles', get_theme_file_uri('./build/index.css'));
 }
