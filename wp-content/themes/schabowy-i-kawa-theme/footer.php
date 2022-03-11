334e1b701b81fd0ba6145f5d0760f37ab6fa77eb
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-11 21:16:49 +0100

    Added footer logo

--- wp-content/themes/schabowy-i-kawa-theme/footer.php
+++ wp-content/themes/schabowy-i-kawa-theme/footer.php
@@ -1,5 +1,8 @@
 <footer class="site-footer">
   <div class="site-footer__navigation_box container">
+    <div class="site-footer__footer_logo">
+      <img class="site-footer__footer_logo--img" src="/wp-content/themes/schabowy-i-kawa-theme/images/logo_doradca_smaku.svg" alt="Logo Schabowy i Kawa">
+    </div>
     <div class="site-footer__site_map">
       <div class="site-footer__link_wrapper">
         <a class="site-footer__link" href="/o-nas">O nas</a>
