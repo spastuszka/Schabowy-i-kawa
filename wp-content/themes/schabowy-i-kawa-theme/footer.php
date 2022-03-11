b0cce5c8cf086db6cd63fb40a1240c8fdcea27c5
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-11 22:17:31 +0100

    Added footer socials in first step

--- wp-content/themes/schabowy-i-kawa-theme/footer.php
+++ wp-content/themes/schabowy-i-kawa-theme/footer.php
@@ -23,6 +23,27 @@
         <a class="site-footer__link" href="/polityka-prywatnosci">Polityka prywatności</a>
       </div>
     </div>
+    <div class="site-footer__footer_socials">
+      <nav>
+        <ul class="min-list social-icons-list group">
+          <li>
+            <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </footer>
 
