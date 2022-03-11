15d5de021af80654c036fbaefad6e764edc6653a
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-11 21:01:23 +0100

    Create basic structre in footer and one element in site map

--- wp-content/themes/schabowy-i-kawa-theme/footer.php
+++ wp-content/themes/schabowy-i-kawa-theme/footer.php
@@ -1,5 +1,11 @@
 <footer class="site-footer">
-
+  <div class="site-footer__navigation_box container">
+    <div class="site-footer__site_map">
+      <div class="site-footer__link_wrapper">
+        <a class="site-footer__link" href="/o-nas">O nas</a>
+      </div>
+    </div>
+  </div>
 </footer>
 
 <?php wp_footer(); ?>
