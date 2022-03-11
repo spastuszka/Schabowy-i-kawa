1752c7c11846de634e17d589e60508007a42536b
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-11 20:17:27 +0100

    Clear style main file and star create own footer

--- wp-content/themes/schabowy-i-kawa-theme/footer.php
+++ wp-content/themes/schabowy-i-kawa-theme/footer.php
@@ -1,61 +1,53 @@
 <footer class="site-footer">
   <div class="site-footer__inner container container--narrow">
-    <div class="group">
-      <div class="site-footer__col-one">
-        <h1 class="school-logo-text school-logo-text--alt-color">
-          <a href="#"><strong>Schabowy</strong> i kawa</a>
-        </h1>
-        <p><a class="site-footer__link" href="tel:123456789">123456789</a></p>
+    <div class="site-footer__col-two-three-group">
+      <div class="site-footer__col-two">
+        <h3 class="headline headline--small">Strony</h3>
+        <nav class="nav-list">
+          <ul>
+            <li><a href="#">O nas</a></li>
+            <li><a href="#">Przepisy</a></li>
+            <li><a href="#">Porady</a></li>
+            <li><a href="#">Kontakt</a></li>
+          </ul>
+        </nav>
       </div>
 
-      <div class="site-footer__col-two-three-group">
-        <div class="site-footer__col-two">
-          <h3 class="headline headline--small">Strony</h3>
-          <nav class="nav-list">
-            <ul>
-              <li><a href="#">O nas</a></li>
-              <li><a href="#">Przepisy</a></li>
-              <li><a href="#">Porady</a></li>
-              <li><a href="#">Kontakt</a></li>
-            </ul>
-          </nav>
-        </div>
-
-        <div class="site-footer__col-three">
-          <h3 class="headline headline--small">W skrócie</h3>
-          <nav class="nav-list">
-            <ul>
-              <li><a href="#">Regulamin</a></li>
-              <li><a href="#">Polityka prywatności</a></li>
-              <li><a href="#">Mapa serwisu</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-
-      <div class="site-footer__col-four">
-        <h3 class="headline headline--small">Udostępnij</h3>
-        <nav>
-          <ul class="min-list social-icons-list group">
-            <li>
-              <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-            </li>
-            <li>
-              <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-            </li>
-            <li>
-              <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-            </li>
-            <li>
-              <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-            </li>
-            <li>
-              <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-            </li>
+      <div class="site-footer__col-three">
+        <h3 class="headline headline--small">W skrócie</h3>
+        <nav class="nav-list">
+          <ul>
+            <li><a href="#">Regulamin</a></li>
+            <li><a href="#">Polityka prywatności</a></li>
+            <li><a href="#">Mapa serwisu</a></li>
           </ul>
         </nav>
       </div>
     </div>
+
+    <div class="site-footer__col-four">
+      <h3 class="headline headline--small">Udostępnij</h3>
+      <nav>
+        <ul class="min-list social-icons-list group">
+          <li>
+            <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+          </li>
+          <li>
+            <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
   </div>
 </footer>
 
