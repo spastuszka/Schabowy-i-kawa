95f8bfcbbb9f5a91e8fec575f273610b6f20a0be
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-12 22:49:47 +0100

    Added structure and style site header menu

--- wp-content/themes/schabowy-i-kawa-theme/header.php
+++ wp-content/themes/schabowy-i-kawa-theme/header.php
@@ -8,10 +8,25 @@
 <body>
   <header class="site-header">
     <div class="container">
-      <h1 class="float-left">
+      <h1 class="site-logo-text float-left">
         <a href="#"><strong>Schabowy</strong> i kawa</a>
       </h1>
       <span class="site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+      <div class="site-header__menu group">
+        <nav class="main-navigation">
+          <ul>
+            <li><a href="#">O nas</a></li>
+            <li><a href="#">rzepisy</a></li>
+            <li><a href="#">Porady</a></li>
+            <li><a href="#">Kontakt</a></li>
+          </ul>
+        </nav>
+        <div class="site-header__util">
+          <a href="#" class="btn btn--medium btn--dark-red float-left push-right">Zaloguj</a>
+          <a href="#" class="btn btn--medium btn--pink float-left">Rejestracja</a>
+          <span class="search-trigger js-search-trigger push-right"><i class="fa fa-search" aria-hidden="true"></i></span>
+        </div>
+      </div>
     </div>
   </header>
