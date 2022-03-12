54296f15441409eab5b5b83de608bec0904e816b
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-12 21:37:20 +0100

    Added searc trigger

--- wp-content/themes/schabowy-i-kawa-theme/header.php
+++ wp-content/themes/schabowy-i-kawa-theme/header.php
@@ -9,7 +9,9 @@
   <header class="site-header">
     <div class="container">
       <h1 class="float-left">
-        <a href="#"><strong>Schabowy</strong>i kawa</a>
+        <a href="#"><strong>Schabowy</strong> i kawa</a>
       </h1>
+      <span class="site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+      <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
     </div>
   </header>
