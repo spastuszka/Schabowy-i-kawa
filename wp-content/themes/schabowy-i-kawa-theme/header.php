460780e3851c8b0ed01049522866d8de251891d9
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-13 12:33:09 +0100

    Added logo svg to header and some style about it

--- wp-content/themes/schabowy-i-kawa-theme/header.php
+++ wp-content/themes/schabowy-i-kawa-theme/header.php
@@ -9,7 +9,7 @@
   <header class="site-header">
     <div class="container">
       <h1 class="site-logo-text float-left">
-        <a href="#"><strong>Schabowy</strong> i kawa</a>
+        <a href="#"><img class="site-logo--img" src="<?php echo get_theme_file_uri('/images/logo_doradca_smaku.svg'); ?>" alt="Logo Schabowy i Kawa"></a>
       </h1>
       <span class="site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
