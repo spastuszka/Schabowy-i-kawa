8f91069781eb226c24afe5ab1a745a33b2f8f90d
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-13 11:45:58 +0100

    Created structure about page banner in front page

--- wp-content/themes/schabowy-i-kawa-theme/index.php
+++ wp-content/themes/schabowy-i-kawa-theme/index.php
@@ -1,11 +1,14 @@
-<?php get_header();
+<?php get_header(); ?>
 
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(/wp-content/themes/schabowy-i-kawa-theme/images/cooking.jpg)"></div>
+  <div class="page-banner__content container t-center c-white">
+    <h1 class="headline headline--large">Witam!</h1>
+    <h2 class="headline headline--medium">Dziękuję za obejrzenie mojej strony.</h2>
+    <h3 class="headline headline--small">Może znajdziesz tutaj przepis idealny dla <strong>siebie</strong>?</h3>
+    <a href="#" class="btn btn--large btn--blue">Pomysły na śniadania</a>
+    <a href="#" class="btn btn--large btn--blue">Program</a>
+  </div>
+</div>
 
-while (have_posts()) {
-  the_post(); ?>
-
-  <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-  <p><?php the_content(); ?></p>
-
-<?php }
-get_footer(); ?>
+<?php get_footer(); ?>
