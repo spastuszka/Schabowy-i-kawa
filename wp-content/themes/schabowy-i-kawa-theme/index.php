8dc2039a35f3ca23439d30b1512f431b6d9b2fde
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-13 13:23:55 +0100

    Added recipe section in front page - basic

--- wp-content/themes/schabowy-i-kawa-theme/index.php
+++ wp-content/themes/schabowy-i-kawa-theme/index.php
@@ -11,4 +11,19 @@
   </div>
 </div>
 
+<div class="full-width-split group">
+  <h2 class="headline headline--large-medium t-left t-dark">Pomysły na pyszne przekąski</h2>
+
+  <div class="recipe-summary">
+    <a class="event-summary__date t-center" href="#">
+      <span class="event-summary__month">Mar</span>
+      <span class="event-summary__day">25</span>
+    </a>
+    <div class="event-summary__content">
+      <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
+      <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+    </div>
+  </div>
+</div>
+
 <?php get_footer(); ?>
