2e168a7b0f8e141be5dc625f117b1e697f1dca19
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-17 20:10:47 +0100

    Created front page html and css recipe summay section

--- wp-content/themes/schabowy-i-kawa-theme/index.php
+++ wp-content/themes/schabowy-i-kawa-theme/index.php
@@ -15,9 +15,86 @@
   <h2 class="headline headline--large-medium t-left t-dark">Polecane przepisy</h2>
 
   <div class="recipe-summary">
-    <div class="event-summary__content">
-      <h5 class="event-summary__title headline headline--tiny t-left t-dark"><a href="#">Przepis 1</a></h5>
-      <p class="t-left t-dark">Krótki opis do przepisu <a href="#" class="nu gray">Zobacz więcej</a></p>
+    <div class="recipe-summary__item">
+      <div class="recipe-summary__item--inner">
+        <div class="recipe-summary__item--wrapper">
+          <a href="#" class="img_wrapper">
+            <picture>
+              <source type="image/webp" srcset="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.webp" data-srcset="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.webp" class="" media="(min-width: 400px)">
+              <img alt="Sałatka z zupek chińskich foto" class="lazy loaded" src="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.jpg" data-src="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.jpg" data-was-processed="true">
+            </picture>
+          </a>
+        </div>
+        <div class="recipe-summary__item--description">
+          <div class="rec">
+            <h4 class="item__title t-dark no-margin">Sałatka meksykańska</h4>
+          </div>
+          <div class="item__stats">
+            <span class="stats__duration">
+              <span class="stats__duration--clock"></span>
+              <span class="stats__duration--text t-dark">19 min</span>
+            </span>
+            <span class="stats__difficulty">
+              <span class="stats__difficulty--icon"></span>
+              <span class="stats__difficulty--text t-dark">Łatwy</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="recipe-summary__item">
+      <div class="recipe-summary__item--inner">
+        <div class="recipe-summary__item--wrapper">
+          <a href="#" class="img_wrapper">
+            <picture>
+              <source type="image/webp" srcset="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.webp" data-srcset="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.webp" class="" media="(min-width: 400px)">
+              <img alt="Sałatka z zupek chińskich foto" class="lazy loaded" src="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.jpg" data-src="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.jpg" data-was-processed="true">
+            </picture>
+          </a>
+        </div>
+        <div class="recipe-summary__item--description">
+          <div class="rec">
+            <h4 class="item__title t-dark no-margin">Sałatka meksykańska</h4>
+          </div>
+          <div class="item__stats">
+            <span class="stats__duration">
+              <span class="stats__duration--clock"></span>
+              <span class="stats__duration--text t-dark">19 min</span>
+            </span>
+            <span class="stats__difficulty">
+              <span class="stats__difficulty--icon"></span>
+              <span class="stats__difficulty--text t-dark">Łatwy</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="recipe-summary__item">
+      <div class="recipe-summary__item--inner">
+        <div class="recipe-summary__item--wrapper">
+          <a href="#" class="img_wrapper">
+            <picture>
+              <source type="image/webp" srcset="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.webp" data-srcset="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.webp" class="" media="(min-width: 400px)">
+              <img alt="Sałatka z zupek chińskich foto" class="lazy loaded" src="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.jpg" data-src="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.jpg" data-was-processed="true">
+            </picture>
+          </a>
+        </div>
+        <div class="recipe-summary__item--description">
+          <div class="rec">
+            <h4 class="item__title t-dark no-margin">Sałatka meksykańska</h4>
+          </div>
+          <div class="item__stats">
+            <span class="stats__duration">
+              <span class="stats__duration--clock"></span>
+              <span class="stats__duration--text t-dark">19 min</span>
+            </span>
+            <span class="stats__difficulty">
+              <span class="stats__difficulty--icon"></span>
+              <span class="stats__difficulty--text t-dark">Łatwy</span>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
