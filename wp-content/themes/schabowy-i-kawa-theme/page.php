cb2fb852b06922793d50705f6f7aebc4899f3a80
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-21 21:39:48 +0100

    Modify logc back to parent page and display title in metabox

--- wp-content/themes/schabowy-i-kawa-theme/page.php
+++ wp-content/themes/schabowy-i-kawa-theme/page.php
@@ -17,10 +17,11 @@ while (have_posts()) {
   <div class="container container--narrow page-section">
 
     <?php
-    if (wp_get_post_parent_id(get_the_ID())) { ?>
+    $theParent = wp_get_post_parent_id(get_the_ID());
+    if ($theParent) { ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span>
+          <a class="metabox__blog-home-link" href="<?php echo the_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
     <?php } ?>
