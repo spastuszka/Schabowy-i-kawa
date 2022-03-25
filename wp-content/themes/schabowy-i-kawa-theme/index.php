23ce0e1534c9c26323b891cb325012fea13c4b1d
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-25 20:29:43 +0100

    Added initial logic in blog page

--- wp-content/themes/schabowy-i-kawa-theme/index.php
+++ wp-content/themes/schabowy-i-kawa-theme/index.php
@@ -3,13 +3,18 @@
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?php the_title(); ?></h1>
+    <h1 class="page-banner__title">Blog kulinarny</h1>
     <div class="page-banner__intro c-white">
       <p>Zobacz nasze najnowsze nowinki ze świata kuchni.</p>
     </div>
   </div>
 </div>
 
-<div class="container container--narrow page-section">test123</div>
+<div class="container container--narrow page-section">
+  <?php while (have_posts()) {
+    the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+  <?php } ?>
+</div>
 
 <?php get_footer(); ?>
