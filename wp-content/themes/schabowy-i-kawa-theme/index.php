123ce66148f0763c75f6986f01b537edb26e6b5d
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-03-27 19:14:31 +0200

    Added  title with link in blog page

--- wp-content/themes/schabowy-i-kawa-theme/index.php
+++ wp-content/themes/schabowy-i-kawa-theme/index.php
@@ -13,7 +13,10 @@
 <div class="container container--narrow page-section">
   <?php while (have_posts()) {
     the_post(); ?>
-    <h1><?php the_title(); ?></h1>
+    <div class="post-item">
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    </div>
+
   <?php } ?>
 </div>
 
