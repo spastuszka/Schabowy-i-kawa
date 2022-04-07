227ef4c4bcdc3cb69b7c52505e9d6d11c6a49d42
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-04-07 07:27:55 +0200

    Added custom post type file and add one cpt - recipe

---
+++ wp-content/mu-plugins/cooker_post_types.php
@@ -0,0 +1,21 @@
+<?php
+
+/* Custom Post Type */
+
+function cooker_post_types()
+{
+  register_post_type('recipe', array(
+    'public' => true,
+    'description' => 'Recipe custom post type',
+    'labels' => array(
+      'name' => 'Recipes',
+      'add_new_item' => 'Add New Recipe',
+      'edit_item' => 'Edit Recipe',
+      'all_items' => 'All Recipes',
+      'singular_name' => 'Recipe',
+    ),
+    'menu_icon' => 'dashicons-food',
+  ));
+}
+
+add_action('init', 'cooker_post_types');
