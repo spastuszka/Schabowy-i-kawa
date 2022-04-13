3b0308305ce47787dd2d61dbb46d5d84d89d3f8c
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-04-13 06:11:08 +0200

    Added dynamic field into recipes in archive

--- wp-content/themes/schabowy-i-kawa-theme/archive-recipe.php
+++ wp-content/themes/schabowy-i-kawa-theme/archive-recipe.php
@@ -29,11 +29,11 @@
           <div class="item__stats">
             <span class="stats__duration">
               <span class="stats__duration--clock"></span>
-              <span class="stats__duration--text t-dark">19 min</span>
+              <span class="stats__duration--text t-dark"><?php echo get_field('cooking_time') . ' min'; ?></span>
             </span>
             <span class="stats__difficulty">
               <span class="stats__difficulty--icon"></span>
-              <span class="stats__difficulty--text t-dark">Łatwy</span>
+              <span class="stats__difficulty--text t-dark"><?php the_field('recipe_difficulty_level'); ?></span>
             </span>
           </div>
         </div>
