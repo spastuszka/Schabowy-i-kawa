d218509c7db9cb8eee06f719a0f9873b3d980c17
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-04-27 05:54:00 +0200

    Style single cooer template

--- wp-content/themes/schabowy-i-kawa-theme/single-cooker.php
+++ wp-content/themes/schabowy-i-kawa-theme/single-cooker.php
@@ -16,10 +16,35 @@ while (have_posts()) {
     </div>
   </div>
 
+
+
   <div class="container container--narrow page-section">
-    <div class="breadcrumb"><?php get_breadcrumb(); ?></div>
-    <div class="generic-content">
-      <?php the_content(); ?>
+    <div class="user_cooker_info">
+      <div class="user_promo_card">
+        <div class="user_promo_card--inner">
+          <a href="<?php the_permalink(); ?>">
+            <img class="default_avatar_icon" src="<?php echo get_theme_file_uri('/images/default_avatar_black.svg'); ?>" alt="Default">
+          </a>
+          <div class="user_info">
+            <a class="user_name" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            <div class="user_profile_statistics">
+              <div class="statistics">
+                <img class="icon add_recipe_icon_svg" src="<?php echo get_theme_file_uri('/images/ksiazka.svg'); ?>" alt="Liczba przepisów dodanych przez użytkownika" />
+                <p>0</p>
+              </div>
+              <div class="statistics">
+                <img class="icon black_heart_icon_svg" src="<?php echo get_theme_file_uri('/images/serce_black.svg'); ?>" alt="Ilość lajków od zarejestrowanych i niezarejestrowanych w serwisie użytkowników" />
+                <p>0</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="user_promo_desc">
+        <div class="user_promo_desc--inner">
+          <?php the_content(); ?>
+        </div>
+      </div>
     </div>
   </div>
 <?php }
