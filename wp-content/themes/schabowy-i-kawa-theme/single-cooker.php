21b15ecae40e37f42d3bb042f9a7f061ffcf7d79
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-05-05 18:44:39 +0200

    Single recipe styling and cooker relate recipes

--- wp-content/themes/schabowy-i-kawa-theme/single-cooker.php
+++ wp-content/themes/schabowy-i-kawa-theme/single-cooker.php
@@ -44,6 +44,49 @@ while (have_posts()) {
         <div class="user_promo_desc--inner">
           <?php the_content(); ?>
         </div>
+        <div>
+          <h3>Przepisy:</h3>
+          <?php
+
+          $homepageRecipes = new WP_Query(array(
+            'posts_per_page' => 2,
+            'post_type' => 'recipe',
+            'meta_key' => 'recipe_difficulty_level',
+            'orderby' => 'meta_value',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'related_cookers',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"',
+              )
+            )
+          ));
+
+
+          while ($homepageRecipes->have_posts()) {
+            $homepageRecipes->the_post(); ?>
+            <div class="recipe-summary__item">
+              <div class="recipe-summary__item--inner">
+                <div class="recipe-summary__item--wrapper">
+                  <a href="<?php the_permalink(); ?>" class="img_wrapper">
+                    <picture>
+                      <source type="image/webp" srcset="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.webp" data-srcset="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.webp" class="" media="(min-width: 400px)">
+                      <img alt="Sałatka z zupek chińskich foto" class="lazy loaded" src="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.jpg" data-src="https://pliki.doradcasmaku.pl/salatka-z-zupek-chinskich91-3.jpg" data-was-processed="true">
+                    </picture>
+                  </a>
+                </div>
+                <div class="recipe-summary__item--description">
+                  <div class="rec">
+                    <h4 class="item__title--cooker t-dark no-margin"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <?php }
+          wp_reset_postdata();
+          ?>
+        </div>
       </div>
     </div>
   </div>
