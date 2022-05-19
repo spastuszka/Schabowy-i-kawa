fd30708d923311afc71525cd178d507e85e415a8
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-05-19 08:03:06 +0200

    Some modification about css to migrate to scss and instal npm dependecies

--- wp-content/themes/schabowy-i-kawa-theme/footer.php
+++ wp-content/themes/schabowy-i-kawa-theme/footer.php
@@ -32,16 +32,16 @@
       <nav>
         <ul class="min-list social-icons-list group">
           <li>
-            <a href="#" class="social-color-facebook"><i class="fa-brands fa-facebook" aria-hidden="true"></i></a>
+            <a href="#" class="social-color--facebook"><i class="fa-brands fa-facebook" aria-hidden="true"></i></a>
           </li>
           <li>
-            <a href="#" class="social-color-youtube"><i class="fa-brands fa-youtube" aria-hidden="true"></i></a>
+            <a href="#" class="social-color--youtube"><i class="fa-brands fa-youtube" aria-hidden="true"></i></a>
           </li>
           <li>
-            <a href="#" class="social-color-linkedin"><i class="fa-brands fa-linkedin-in" aria-hidden="true"></i></a>
+            <a href="#" class="social-color--linkedin"><i class="fa-brands fa-linkedin-in" aria-hidden="true"></i></a>
           </li>
           <li>
-            <a href="#" class="social-color-instagram"><i class="fa-brands fa-instagram" aria-hidden="true"></i></a>
+            <a href="#" class="social-color--instagram"><i class="fa-brands fa-instagram" aria-hidden="true"></i></a>
           </li>
         </ul>
       </nav>
