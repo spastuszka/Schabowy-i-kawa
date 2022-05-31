79afbfe63848dd7b5b43169d4f07e44384a3704c
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-05-31 07:55:54 +0200

    Added custom jquery code that opens and closes the search engine window

--- wp-content/themes/schabowy-i-kawa-theme/src/modules/Search.js
+++ wp-content/themes/schabowy-i-kawa-theme/src/modules/Search.js
@@ -1,5 +1,27 @@
+import $ from 'jquery'
+
 class Search {
-  constructor() {}
+  // 1. Opis naszego obiektu oraz jego inicjalizacja
+  constructor() {
+    this.openButton = $('.js-search-trigger')
+    this.closeButton = $('.search-overlay__close')
+    this.searchOverlay = $('.search-overlay')
+    this.events()
+  }
+
+  // 2. Zdarzenia - np. kliknięcie, najechanie itp.
+  events() {
+    this.openButton.on('click', this.openOverlay.bind(this))
+    this.closeButton.on('click', this.closeOverlay.bind(this))
+  }
+
+  // 3. Metody
+  openOverlay() {
+    this.searchOverlay.addClass('search-overlay--active')
+  }
+  closeOverlay() {
+    this.searchOverlay.removeClass('search-overlay--active')
+  }
 }
 
 export default Search
