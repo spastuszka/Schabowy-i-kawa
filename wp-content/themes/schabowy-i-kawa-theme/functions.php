58c1aab63bad8679292b6c964275ebb029edf381
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-06-07 07:39:36 +0200

    Added to default post rest rout new field about author name

--- wp-content/themes/schabowy-i-kawa-theme/functions.php
+++ wp-content/themes/schabowy-i-kawa-theme/functions.php
@@ -1,5 +1,13 @@
 <?php
 
+function cooking_custom_rest(){
+  register_rest_field('post','authorName',array(
+    'get_callback' => function(){ return get_the_author();}
+  ));
+}
+
+add_action('rest_api_init', 'cooking_custom_rest');
+
 /* Funkcja - Page Banner */
 function pageBanner($args = NULL)
 {
