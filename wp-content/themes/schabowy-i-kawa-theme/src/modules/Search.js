bede678890a307d94fc2d9831c3c331f2adef95e
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-06-09 20:09:48 +0200

    Commented unnecessary search results code

--- wp-content/themes/schabowy-i-kawa-theme/src/modules/Search.js
+++ wp-content/themes/schabowy-i-kawa-theme/src/modules/Search.js
@@ -117,12 +117,46 @@ class Search {
           </div>
         `)
         this.isSpinnerVisible = false
-      },
-      () => {
-        //obsluga bledow, jezeli cos nie gra
-        this.searchResults.html('Nieoczekiwany błąd, proszę spróbować ponownie')
       }
     )
+    //stare - pozniej mozna zakomwntowac
+    //asynchroniczne wykonanie search'a
+    // $.when(
+    //   $.getJSON(
+    //     cookingData.root_url +
+    //       '/wp-json/wp/v2/posts?search=' +
+    //       this.searchField.val()
+    //   ),
+    //   $.getJSON(
+    //     cookingData.root_url +
+    //       '/wp-json/wp/v2/pages?search=' +
+    //       this.searchField.val()
+    //   )
+    // ).then(
+    //   (posts, pages) => {
+    //     let combinedResults = posts[0].concat(pages[0])
+    //     this.searchResults.html(`
+    //       <h2 class="search-overlay__section-title">Przepisy</h2>
+    //       ${
+    //         combinedResults.length
+    //           ? '<ul class="link-list min-list">'
+    //           : '<p>Brak informacji</p>'
+    //       }
+    //       ${combinedResults
+    //         .map(
+    //           (item) =>
+    //             `<li><a href="${item.link}">${item.title.rendered}</a></li>`
+    //         )
+    //         .join('')}
+    //       ${combinedResults.length ? '</ul>' : ''}
+    //     `)
+    //     this.isSpinnerVisible = false
+    //   },
+    //   () => {
+    //     //obsluga bledow, jezeli cos nie gra
+    //     this.searchResults.html('Nieoczekiwany błąd, proszę spróbować ponownie')
+    //   }
+    // )
   }
 
   openOverlay() {
