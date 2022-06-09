35f5f26acffcc91e9daf6f702a00af4a6900e7c4
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-06-09 05:38:17 +0200

    Cretead JSON results of earlier created rest route function

--- wp-content/themes/schabowy-i-kawa-theme/include/search-route.php
+++ wp-content/themes/schabowy-i-kawa-theme/include/search-route.php
@@ -9,6 +9,21 @@ function cookerRegSearch(){
 
 add_action('rest_api_init','cookerRegSearch');
 
-function cookingSearchResults(){
-  return 'Created Test Route';
+function cookingSearchResults($data){
+  $cookers = new WP_Query(array(
+    'post_type' => 'cooker',
+    's' => sanitize_text_field($data['term']),
+  ));
+
+  $cookersResults = array();
+
+  while($cookers -> have_posts()){
+    $cookers->the_post();
+    array_push($cookersResults, array(
+      'title' => get_the_title(),
+      'permalink' => get_the_permalink(),
+    ));
+  }
+
+  return $cookersResults;
 }
