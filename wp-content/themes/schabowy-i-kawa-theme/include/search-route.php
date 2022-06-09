7839648f696972931ccc7dd80a708361edc8b26a
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-06-09 08:28:47 +0200

    Separated different result to different arrays according to post type

--- wp-content/themes/schabowy-i-kawa-theme/include/search-route.php
+++ wp-content/themes/schabowy-i-kawa-theme/include/search-route.php
@@ -11,7 +11,7 @@ add_action('rest_api_init','cookerRegSearch');
 
 function cookingSearchResults($data){
   $mainQuery = new WP_Query(array(
-    'post_type' => array('post','pages','cooker','recipe'),
+    'post_type' => array('post','page','cooker','recipe'),
     's' => sanitize_text_field($data['term']),
   ));
 
@@ -22,12 +22,40 @@ function cookingSearchResults($data){
     'recipeInfo' => array(),
   );
 
+
+
   while($mainQuery -> have_posts()){
     $mainQuery->the_post();
-    array_push($searchQueryResults, array(
-      'title' => get_the_title(),
-      'permalink' => get_the_permalink(),
-    ));
+
+    //pushowanie do odpowiedniej tablicy w glownej tablicy rezultatow
+    if(get_post_type() == 'post'){
+      array_push($searchQueryResults['postInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+
+    if(get_post_type() == 'page'){
+      array_push($searchQueryResults['pageInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+
+    if(get_post_type() == 'cooker'){
+      array_push($searchQueryResults['cookerInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+
+    if(get_post_type() == 'recipe'){
+      array_push($searchQueryResults['recipeInfo'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+      ));
+    }
+    
   }
 
   return $searchQueryResults;
