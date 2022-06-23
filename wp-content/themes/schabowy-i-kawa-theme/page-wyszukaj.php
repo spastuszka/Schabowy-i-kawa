f4527004caa4a67152b1c20d31ed0dfed5a7be45
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-06-23 07:22:27 +0200

    Styling abotu search page

--- wp-content/themes/schabowy-i-kawa-theme/page-wyszukaj.php
+++ wp-content/themes/schabowy-i-kawa-theme/page-wyszukaj.php
@@ -50,9 +50,12 @@ while (have_posts()) {
     <?php } ?>
 
     <div class="generic-content">
-      <form method='get' action="<?php echo esc_url(site_url('/'))?>">
-        <input type="search" name="s">
-        <input type="submit" value="Search">
+      <form class='search-form' method='get' action="<?php echo esc_url(site_url('/'))?>">
+        <label class="headline headline--medium" for="s">Nowe wyszukiwanie:</label>
+        <div class="search-form-row">
+          <input class="s" id="s" type="search" name="s">
+          <input class="search-submit" type="submit" value="Wyszukaj">
+        </div>
       </form>
     </div>
   </div>
