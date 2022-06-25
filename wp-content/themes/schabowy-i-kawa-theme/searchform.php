4026cbbeac3de38de23a7bc4769082af1b92dbd6
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-06-25 11:39:45 +0200

    Searchform.php - fix from merge

---
+++ wp-content/themes/schabowy-i-kawa-theme/searchform.php
@@ -0,0 +1,7 @@
+<form class='search-form' method='get' action="<?php echo esc_url(site_url('/'))?>">
+  <label class="headline headline--medium" for="s">Nowe wyszukiwanie:</label>
+  <div class="search-form-row">
+    <input placeholder="Czego szukasz? Przepisu? Porady?" class="s" id="s" type="search" name="s">
+      <input class="search-submit" type="submit" value="Wyszukaj">
+  </div>
+</form>
