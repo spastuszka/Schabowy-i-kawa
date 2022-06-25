1a2e66585372de438b5c399130a0c16ae3d6b2bc
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-06-25 15:23:45 +0200

    Deleted front page button

--- wp-content/themes/schabowy-i-kawa-theme/front-page.php
+++ wp-content/themes/schabowy-i-kawa-theme/front-page.php
@@ -6,7 +6,6 @@
     <h1 class="headline headline--large">Witam!</h1>
     <h2 class="headline headline--medium">Dziękuję za obejrzenie mojej strony.</h2>
     <h3 class="headline headline--small">Może znajdziesz tutaj przepis idealny dla <strong>siebie</strong>?</h3>
-    <a href="#" class="btn btn--large btn--pink push-right">Pomysły na śniadania</a>
     <a href="https://www.linkedin.com/in/sebastian-pastuszka/" class="btn btn--large btn--dark-red" target="_blank">LinkedIn</a>
   </div>
 </div>
