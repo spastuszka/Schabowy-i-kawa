490792f50f105e8ce7e31b21da7770fabb47ce5e
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-06-25 06:41:33 +0200

    Some fixes

--- wp-content/themes/schabowy-i-kawa-theme/header.php
+++ wp-content/themes/schabowy-i-kawa-theme/header.php
@@ -11,9 +11,9 @@
   <header class="site-header">
     <div class="container">
       <h1 class="site-logo--text float-left">
-      <a href="<?php echo esc_url(site_url('/')) ?>" aria-hidden="true">Schnitzel & Coffee</a>
+        Schnitzel & Coffee
       </h1>
-      <a href="<?php echo esc_url(site_url('/wyszukaj')) ?>" class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
+      <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
@@ -28,7 +28,7 @@
         <div class="site-header__util">
           <a href="#" class="btn btn--medium btn--dark-red float-left push-right">Zaloguj</a>
           <a href="#" class="btn btn--medium btn--pink float-left">Rejestracja</a>
-          <a href="<?php echo esc_url(site_url('/wyszukaj')) ?>" class="search-trigger js-search-trigger push-right"><i class="fa-solid fa-magnifying-glass" aria-hidden="true"></i></a>
+          <span class="search-trigger js-search-trigger push-right"><i class="fa-solid fa-magnifying-glass" aria-hidden="true"></i></span>
         </div>
       </div>
     </div>
--- wp-content/themes/schabowy-i-kawa-theme/src/modules/Search.js
+++ wp-content/themes/schabowy-i-kawa-theme/src/modules/Search.js
@@ -1,243 +1,161 @@
-import axios from 'axios'
+import $ from 'jquery'
 
 class Search {
   // 1. Opis naszego obiektu oraz jego inicjalizacja
   constructor() {
     this.addSearchHTML()
-    this.openButton = document.querySelectorAll('.js-search-trigger')
-    this.closeButton = document.querySelector('.search-overlay__close')
-    this.searchOverlay = document.querySelector('.search-overlay')
-    this.searchField = document.querySelector('#search-term')
-    this.searchResults = document.querySelector('#search-overlay__results')
+    this.openButton = $('.js-search-trigger')
+    this.closeButton = $('.search-overlay__close')
+    this.searchOverlay = $('.search-overlay')
+    this.searchField = $('#search-term')
+    this.searchResults = $('#search-overlay__results')
+    this.events()
     this.isOverlayOpen = false
+    this.typingTimer
     this.isSpinnerVisible = false
     this.previousValue
-    this.typingTimer
-    this.events()
   }
 
   // 2. Zdarzenia - np. kliknięcie, najechanie itp.
   events() {
-    this.openButton.forEach((el) => {
-      el.addEventListener('click', (e) => {
-        e.preventDefault()
-        this.openOverlay()
-      })
-    })
-    this.closeButton.addEventListener('click', () => this.closeOverlay())
-    document.addEventListener('keydown', (e) => this.keyPressSearch(e))
-    this.searchField.addEventListener('keyup', () => this.typingLogic())
+    this.openButton.on('click', this.openOverlay.bind(this))
+    this.closeButton.on('click', this.closeOverlay.bind(this))
+    $(document).on('keydown', this.keyPressSearch.bind(this))
+    this.searchField.on('keyup', this.typingLogic.bind(this))
   }
 
   // 3. Metody
   typingLogic() {
     //warunek logiczny uruchamiajacy wyszukiwanie wtedy kiedy wartos pola wyszukiwania sie zmienia
-    if (this.searchField.value != this.previousValue) {
+    if (this.searchField.val() != this.previousValue) {
       //Czyszczenie wcześniej uruchomienego timera w zmiennej typingTimer
       clearTimeout(this.typingTimer)
 
-      if (this.searchField.value) {
+      if (this.searchField.val()) {
         if (!this.isSpinnerVisible) {
-          this.searchResults.innerHTML = '<div class="loader"></div>'
+          this.searchResults.html('<div class="loader"></div>')
           this.isSpinnerVisible = true
         }
         this.typingTimer = setTimeout(this.getResults.bind(this), 750)
       } else {
-        this.searchResults.innerHTML = ''
+        this.searchResults.html(' ')
         this.isSpinnerVisible = false
       }
     }
     //Ustanowienie zmiennej ktora bedzie przechowywac wartosc pola wyszuykiwania
-    this.previousValue = this.searchField.value
+    this.previousValue = this.searchField.val()
   }
 
-  async getResults() {
-    try {
-      const response = await axios.get(
-        cookingData.root_url +
-          '/wp-json/cookers/v1/search?term=' +
-          this.searchField.value
-      )
-      const results = response.data
-      this.searchResults.innerHTML = `
-      <div class="row">
-        <div class="one-quarter">
-          <h2 class="search-overlay__section-title">Strony</h2>
-          ${
-            results.pageInfo.length
-              ? '<ul class="link-list min-list">'
-              : '<p>Brak informacji</p>'
-          }
-          ${results.pageInfo
-            .map(
-              (item) => `<li><a href="${item.permalink}">${item.title}</a></li>`
-            )
-            .join('')}
-          ${results.pageInfo.length ? '</ul>' : ''}
-        </div>
-        <div class="one-quarter">
-          <h2 class="search-overlay__section-title">Porady</h2>
-          ${
-            results.postInfo.length
-              ? '<ul class="link-list min-list">'
-              : '<p>Brak informacji</p>'
-          }
-          ${results.postInfo
-            .map(
-              (item) => `<li><a href="${item.permalink}">${item.title}</a></li>`
-            )
-            .join('')}
-          ${results.postInfo.length ? '</ul>' : ''}
-        </div>
-        <div class="one-quarter">
-          <h2 class="search-overlay__section-title">Przepisy</h2>
-          ${
-            results.recipeInfo.length
-              ? '<ul class="link-list min-list">'
-              : '<p>Brak informacji</p>'
-          }
-          ${results.recipeInfo
-            .map(
-              (item) => `<li><a href="${item.permalink}">${item.title}</a></li>`
-            )
-            .join('')}
-          ${results.recipeInfo.length ? '</ul>' : ''}
-        </div>
-        <div class="one-quarter">
-          <h2 class="search-overlay__section-title">Kucharze</h2>
-          ${
-            results.cookerInfo.length
-              ? '<ul class="link-list min-list">'
-              : '<p>Brak informacji</p>'
-          }
-          ${results.cookerInfo
-            .map(
-              (item) => `<li><a href="${item.permalink}">${item.title}</a></li>`
-            )
-            .join('')}
-          ${results.cookerInfo.length ? '</ul>' : ''}
-        </div>
-      </div>
-    `
-      this.isSpinnerVisible = false
-    } catch (e) {
-      console.log(e)
-    }
+  getResults() {
+    $.getJSON(
+      cookingData.root_url +
+        '/wp-json/cookers/v1/search?term=' +
+        this.searchField.val(),
+      (results) => {
+        this.searchResults.html(`
+          <div class="row">
+            <div class="one-quarter">
+              <h2 class="search-overlay__section-title">Strony</h2>
+              ${
+                results.pageInfo.length
+                  ? '<ul class="link-list min-list">'
+                  : '<p>Brak informacji</p>'
+              }
+              ${results.pageInfo
+                .map(
+                  (item) =>
+                    `<li><a href="${item.permalink}">${item.title}</a></li>`
+                )
+                .join('')}
+              ${results.pageInfo.length ? '</ul>' : ''}
+            </div>
+            <div class="one-quarter">
+              <h2 class="search-overlay__section-title">Porady</h2>
+              ${
+                results.postInfo.length
+                  ? '<ul class="link-list min-list">'
+                  : '<p>Brak informacji</p>'
+              }
+              ${results.postInfo
+                .map(
+                  (item) =>
+                    `<li><a href="${item.permalink}">${item.title}</a></li>`
+                )
+                .join('')}
+              ${results.postInfo.length ? '</ul>' : ''}
+            </div>
+            <div class="one-quarter">
+              <h2 class="search-overlay__section-title">Przepisy</h2>
+              ${
+                results.recipeInfo.length
+                  ? '<ul class="link-list min-list">'
+                  : '<p>Brak informacji</p>'
+              }
+              ${results.recipeInfo
+                .map(
+                  (item) =>
+                    `<li><a href="${item.permalink}">${item.title}</a></li>`
+                )
+                .join('')}
+              ${results.recipeInfo.length ? '</ul>' : ''}
+            </div>
+            <div class="one-quarter">
+              <h2 class="search-overlay__section-title">Kucharze</h2>
+              ${
+                results.cookerInfo.length
+                  ? '<ul class="link-list min-list">'
+                  : '<p>Brak informacji</p>'
+              }
+              ${results.cookerInfo
+                .map(
+                  (item) =>
+                    `<li><a href="${item.permalink}">${item.title}</a></li>`
+                )
+                .join('')}
+              ${results.cookerInfo.length ? '</ul>' : ''}
+            </div>
+          </div>
+        `)
+        this.isSpinnerVisible = false
+      },
+      () => {
+        //obsluga bledow, jezeli cos nie gra
+        this.searchResults.html('Nieoczekiwany błąd, proszę spróbować ponownie')
+      }
+    )
   }
-  /*======= JQuery - old*/
-  // getResults() {
-  //   $.getJSON(
-  //     cookingData.root_url +
-  //       '/wp-json/cookers/v1/search?term=' +
-  //       this.searchField.val(),
-  //     (results) => {
-  //       this.searchResults.html(`
-  //         <div class="row">
-  //           <div class="one-quarter">
-  //             <h2 class="search-overlay__section-title">Strony</h2>
-  //             ${
-  //               results.pageInfo.length
-  //                 ? '<ul class="link-list min-list">'
-  //                 : '<p>Brak informacji</p>'
-  //             }
-  //             ${results.pageInfo
-  //               .map(
-  //                 (item) =>
-  //                   `<li><a href="${item.permalink}">${item.title}</a></li>`
-  //               )
-  //               .join('')}
-  //             ${results.pageInfo.length ? '</ul>' : ''}
-  //           </div>
-  //           <div class="one-quarter">
-  //             <h2 class="search-overlay__section-title">Porady</h2>
-  //             ${
-  //               results.postInfo.length
-  //                 ? '<ul class="link-list min-list">'
-  //                 : '<p>Brak informacji</p>'
-  //             }
-  //             ${results.postInfo
-  //               .map(
-  //                 (item) =>
-  //                   `<li><a href="${item.permalink}">${item.title}</a></li>`
-  //               )
-  //               .join('')}
-  //             ${results.postInfo.length ? '</ul>' : ''}
-  //           </div>
-  //           <div class="one-quarter">
-  //             <h2 class="search-overlay__section-title">Przepisy</h2>
-  //             ${
-  //               results.recipeInfo.length
-  //                 ? '<ul class="link-list min-list">'
-  //                 : '<p>Brak informacji</p>'
-  //             }
-  //             ${results.recipeInfo
-  //               .map(
-  //                 (item) =>
-  //                   `<li><a href="${item.permalink}">${item.title}</a></li>`
-  //               )
-  //               .join('')}
-  //             ${results.recipeInfo.length ? '</ul>' : ''}
-  //           </div>
-  //           <div class="one-quarter">
-  //             <h2 class="search-overlay__section-title">Kucharze</h2>
-  //             ${
-  //               results.cookerInfo.length
-  //                 ? '<ul class="link-list min-list">'
-  //                 : '<p>Brak informacji</p>'
-  //             }
-  //             ${results.cookerInfo
-  //               .map(
-  //                 (item) =>
-  //                   `<li><a href="${item.permalink}">${item.title}</a></li>`
-  //               )
-  //               .join('')}
-  //             ${results.cookerInfo.length ? '</ul>' : ''}
-  //           </div>
-  //         </div>
-  //       `)
-  //       this.isSpinnerVisible = false
-  //     },
-  //     () => {
-  //       //obsluga bledow, jezeli cos nie gra
-  //       this.searchResults.html('Nieoczekiwany błąd, proszę spróbować ponownie')
-  //     }
-  //   )
-  // }
 
   openOverlay() {
-    this.searchOverlay.classList.add('search-overlay--active')
-    document.body.classList.add('body-no-scroll')
-    this.searchField.value = ''
-    setTimeout(() => this.searchField.focus(), 301)
-    this.isOverlayOpen = true
-    return false
+    this.searchOverlay.addClass('search-overlay--active')
+    $('body').addClass('body-no-scroll')
+    this.searchField.val('')
+    setTimeout(() => this.searchField.trigger('focus'), 301)
   }
   closeOverlay() {
-    this.searchOverlay.classList.remove('search-overlay--active')
-    document.body.classList.remove('body-no-scroll')
-    this.isOverlayOpen = false
+    this.searchOverlay.removeClass('search-overlay--active')
+    $('body').removeClass('body-no-scroll')
+    this.searchResults.html('')
   }
 
   keyPressSearch(e) {
     if (
       e.keyCode == 83 &&
       !this.isOverlayOpen &&
-      document.activeElement.tagName != 'INPUT' &&
-      document.activeElement.tagName != 'TEXTAREA'
+      !$('input, textarea').is(':focus')
     ) {
       this.openOverlay()
+      this.isOverlayOpen = true
     }
 
     if (e.keyCode == 27 && this.isOverlayOpen) {
       this.closeOverlay()
+      this.isOverlayOpen = false
     }
   }
 
   //dodanie wyszukiwarki na koniec kodu umieszczajac go do footera
   addSearchHTML() {
-    document.body.insertAdjacentHTML(
-      'beforeend',
-      `
+    $('body').append(`
     <div class="search-overlay">
       <div class="search-overlay__top">
         <div class="container">
@@ -250,8 +168,7 @@ class Search {
         <div id="search-overlay__results"></div>
       </div>
     </div>
-    `
-    )
+    `)
   }
 }
 
