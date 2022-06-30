a711d88d0af323a4c51bdea6253075a6a0772feb
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-06-30 06:07:26 +0200

    Fixed header style

--- wp-content/themes/schabowy-i-kawa-theme/header.php
+++ wp-content/themes/schabowy-i-kawa-theme/header.php
@@ -27,10 +27,10 @@
             <li><a href="#">Kontakt</a></li>
           </ul>
         </nav>
+        <a href="<?php echo esc_url(site_url('/wyszukaj')) ?>" class="search-trigger js-search-trigger push-right"><i class="fa-solid fa-magnifying-glass" aria-hidden="true"></i></a>
         <div class="site-header__util">
           <a href="#" class="btn btn--medium btn--dark-red float-left push-right">Zaloguj</a>
-          <a href="#" class="btn btn--medium btn--pink float-left">Rejestracja</a>
-          <a href="<?php echo esc_url(site_url('/wyszukaj')) ?>" class="search-trigger js-search-trigger push-right"><i class="fa-solid fa-magnifying-glass" aria-hidden="true"></i></a>
+          <a href="#" class="btn btn--medium btn--dark btn--register float-left">Rejestracja</a>
         </div>
       </div>
     </div>
