5a50538601a3694476eea1c0c080fe100a29a166
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-07-09 11:39:23 +0200

    Modified singke article style about gallery and content

--- wp-content/themes/schabowy-i-kawa-theme/single.php
+++ wp-content/themes/schabowy-i-kawa-theme/single.php
@@ -24,12 +24,22 @@ while (have_posts()) {
         <?php $postTitle = sanitize_title(the_title());
         echo $postTitle ?>
       </h2>
+      <p class="article--header__excerpt">
+        <?php
+        $value = get_field("post_inner_excerpt");
+        if ($value) {
+          echo $value;
+        } else {
+          echo '';
+        }
+        ?>
+      </p>
     </div>
     <div class="article--gallery">
-      <?php echo get_the_post_thumbnail($post->ID, 'postThumbImg');
+      <?php echo get_the_post_thumbnail($post->ID, 'full');
       ?>
     </div>
-    <div class="generic-content">
+    <div class="generic-content article--content">
       <?php the_content(); ?>
     </div>
   </div>
