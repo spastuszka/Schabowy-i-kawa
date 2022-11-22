a104a17e56b14a788644be80c36b34ca33772f03
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-11-22 07:01:03 +0100

    Created first test register settings to the plugin

--- wp-content/plugins/word-count-plugin/word-count-plugin.php
+++ wp-content/plugins/word-count-plugin/word-count-plugin.php
@@ -12,7 +12,20 @@ class WordCountAndTimePlugin
   function __construct()
   {
     add_action('admin_menu', array($this, 'adminLinkPage'));
+    add_action('admin_init', array($this, 'settings'));
   }
+  function settings()
+  {
+    register_setting(
+      'wordcountplugin',
+      'wcp_location',
+      array(
+        'sanitize_callback' => 'sanitize_text_field',
+        'default' => '0',
+      )
+    );
+  }
+
   function adminLinkPage()
   {
     add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
