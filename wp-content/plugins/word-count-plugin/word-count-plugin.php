179e0659b09351fcdb0ea58621091019b56028e0
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-11-26 05:52:27 +0100

    Added logic about single field in plugin

--- wp-content/plugins/word-count-plugin/word-count-plugin.php
+++ wp-content/plugins/word-count-plugin/word-count-plugin.php
@@ -16,6 +16,17 @@ class WordCountAndTimePlugin
   }
   function settings()
   {
+    /* Dodanie sekcji, która będzie wyświetlać dodane i zarejestrowane pole */
+    add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
+    /* Dodanie pola ustawień HTML*/
+    add_settings_field(
+      'wcp_location',
+      'Display location',
+      array($this, 'locationHTML'),
+      'word-count-settings-page',
+      'wcp_first_section',
+    );
+    /* Rejestracja pola w bazie danych */
     register_setting(
       'wordcountplugin',
       'wcp_location',
@@ -26,15 +37,38 @@ class WordCountAndTimePlugin
     );
   }
 
+  /* Funkcja dodająca HTML do pola ustawień - location */
+  function locationHTML()
+  {
+?>
+    <select name="wcp_location">
+      <option value="0">Beginning of post</option>
+      <option value="1">End of post</option>
+    </select>
+  <?php
+  }
+
   function adminLinkPage()
   {
     add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
   }
 
+  /* Ogólne opakowanie całej strony */
   function ourHTML()
   { ?>
     <div class="wrap">
       <h1>Word Count Settings</h1>
+      <form action="options.php" method="POST">
+        <?php
+        /* Dodajemy to ustawienie by nie było błędu serjalizacji
+        jest to wskazanie grupy pól rejestrowanych w DB
+        */
+        settings_fields('wordcountplugin');
+        /* Wywietlenie wszystkich powiązanych sekcji z daną stroną */
+        do_settings_sections('word-count-settings-page');
+        submit_button();
+        ?>
+      </form>
     </div>
 <?php
   }
