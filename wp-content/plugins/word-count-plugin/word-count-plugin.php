e0d2c7b9d2c8205eb998ab020f0cb83b5fe6d7b5
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-11-26 06:15:11 +0100

    Added  selected logic to see what anserw is chose in DB

--- wp-content/plugins/word-count-plugin/word-count-plugin.php
+++ wp-content/plugins/word-count-plugin/word-count-plugin.php
@@ -42,8 +42,8 @@ class WordCountAndTimePlugin
   {
 ?>
     <select name="wcp_location">
-      <option value="0">Beginning of post</option>
-      <option value="1">End of post</option>
+      <option value="0" <?php selected(get_option('wcp_location'), '0'); ?>>Beginning of post</option>
+      <option value="1" <?php selected(get_option('wcp_location'), '1'); ?>>End of post</option>
     </select>
   <?php
   }
