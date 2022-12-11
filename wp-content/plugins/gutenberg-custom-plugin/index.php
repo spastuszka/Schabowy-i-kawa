1d723df6ceaf4cf7b050a14bc1013766f7a9e4d3
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-12-11 11:18:54 +0100

    Created initial and test file to new gutnbrg plugin with basic information and simple protected

---
+++ wp-content/plugins/gutenberg-custom-plugin/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+    Plugin Name: Gutenberg Plugin
+    Description: My first plugin about gutenberg custom element
+    Version: 1.0
+    Author: Sebstian Pastuszka
+  */
+
+if (!defined('ABSPATH')) exit; // Zabezpieczenie przed bezpośrednim dostępem do pliku poprzez URL bezpo
