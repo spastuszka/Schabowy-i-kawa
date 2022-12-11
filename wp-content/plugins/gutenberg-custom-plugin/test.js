60940ee063ef4ec73685b877c0e32b04b21603d4
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-12-11 12:52:54 +0100

    Basic structer about JS block gutenberg plugin

--- wp-content/plugins/gutenberg-custom-plugin/test.js
+++ wp-content/plugins/gutenberg-custom-plugin/test.js
@@ -1,1 +1,11 @@
-alert("test");
+wp.blocks.registerBlockType('gutenberg-custom-plugin/test-gutenberg-block',{
+  title: "Gutenberg Block",
+  icon: "smiley",
+  category: "common",
+  edit: function(){
+
+  },
+  save: function(){
+    
+  }
+})
