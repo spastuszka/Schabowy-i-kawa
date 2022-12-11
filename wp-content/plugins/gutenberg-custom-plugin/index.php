03bb46ea4b1f8aa4d863ae91f8746e7400913f9c
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-12-11 12:05:29 +0100

    Loaded tested js file ebout gutenberg blocks

--- wp-content/plugins/gutenberg-custom-plugin/index.php
+++ wp-content/plugins/gutenberg-custom-plugin/index.php
@@ -6,4 +6,20 @@
     Author: Sebstian Pastuszka
   */
 
-if (!defined('ABSPATH')) exit; // Zabezpieczenie przed bezpośrednim dostępem do pliku poprzez URL bezpo
+if (!defined('ABSPATH')) exit; // Zabezpieczenie przed bezpośrednim dostępem do pliku poprzez URL bezpośredni do pliku
+
+class GutenbergCustom
+{
+  function __construct()
+  {
+    // załadowanie testowego pliku JS po stronie admina
+    add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+  }
+
+  function adminAssets()
+  {
+    wp_enqueue_script('ourtestblocktype', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks'));
+  }
+}
+
+$gutenbergCustom = new GutenbergCustom();
