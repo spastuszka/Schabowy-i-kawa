a236960c7f5881fa82c4187515f8cb6a0982477e
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-12-12 14:40:28 +0100

    Changed main index js file where open build plugin block

--- wp-content/plugins/gutenberg-custom-plugin/index.php
+++ wp-content/plugins/gutenberg-custom-plugin/index.php
@@ -18,7 +18,7 @@ class GutenbergCustom
 
   function adminAssets()
   {
-    wp_enqueue_script('ourtestblocktype', plugin_dir_url(__FILE__) . 'test.js', array('wp-blocks', 'wp-element'));
+    wp_enqueue_script('ourtestblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
   }
 }
 
