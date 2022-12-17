3a7e29abd6dfae21d353f9246421c1139c174412
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-12-17 12:17:28 +0100

    Basic information about duplicate plugin

--- wp-content/plugins/quiz-plugin/src/index.js
+++ wp-content/plugins/quiz-plugin/src/index.js
@@ -10,28 +10,30 @@ wp.blocks.registerBlockType('quiz-plugin/gutenberg-block-quiz',{
       type:"string",
     },
   },
-  edit: function(props){
-    
-    function updateSkyColor(e){
-      props.setAttributes({
-        skyColor: e.target.value
-      })
-    }
-
-    function updateGrassColor(e){
-      props.setAttributes({
-        grassColor: e.target.value
-      })
-    }
-
-    return(
-      <div>
-        <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor}/>
-        <input type="text" placeholder="grass color" value={props.attributes.grassColor} onChange={updateGrassColor}/>
-      </div>
-    )
-  },
+  edit: EditComponent,
   save: function(props){
     return null
   },
 })
+
+function EditComponent(props){
+    
+  function updateSkyColor(e){
+    props.setAttributes({
+      skyColor: e.target.value
+    })
+  }
+
+  function updateGrassColor(e){
+    props.setAttributes({
+      grassColor: e.target.value
+    })
+  }
+
+  return(
+    <div>
+      <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor}/>
+      <input type="text" placeholder="grass color" value={props.attributes.grassColor} onChange={updateGrassColor}/>
+    </div>
+  )
+}
