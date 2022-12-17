4a18202789805b864f87c086c75bbb1d0737d78b
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2022-12-17 10:03:14 +0100

    Added modification about PHP return html code, more artistic

--- wp-content/plugins/gutenberg-custom-plugin/index.php
+++ wp-content/plugins/gutenberg-custom-plugin/index.php
@@ -27,7 +27,12 @@ class GutenbergCustom
 
   function theHTML($attributes)
   {
-    return '<p>Today the sky is ' . $attributes['skyColor'] . ' and the grass is ' . $attributes['grassColor'] . '.!!!</p>';
+    // return '<p>Today the sky is ' . $attributes['skyColor'] . ' and the grass is ' . $attributes['grassColor'] . '.!!!</p>';
+    ob_start(); ?>
+    <h3>Today the sky is <?php echo esc_html($attributes['skyColor']) ?> and the grass is <?php echo esc_html($attributes['grassColor']) ?>!</h3>
+
+<?php
+    return ob_get_clean();
   }
 }
 
