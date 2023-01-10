63b6abd3206e466361bb32d117817f67d7cc8852
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2023-01-10 05:22:40 +0100

    selecting a file and using it in the gutenberg editing screen

--- wp-content/plugins/quiz-plugin/index.php
+++ wp-content/plugins/quiz-plugin/index.php
@@ -18,9 +18,11 @@ class QuizCustom
 
   function adminAssets()
   {
+    wp_register_style('quizeditcss', plugin_dir_url(__FILE__) . 'build/index.css');
     wp_register_script('quizblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element', 'wp-editor'));
     register_block_type('quiz-plugin/gutenberg-block-quiz', array(
       'editor_script' => 'quizblocktype',
+      'editor_style' => 'quizeditcss',
       'render_callback' => array($this, 'theHTML')
     ));
   }
