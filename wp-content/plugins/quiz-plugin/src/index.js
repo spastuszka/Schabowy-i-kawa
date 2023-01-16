18607859936679d4d8998be85d5f1f46481b5d07
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2023-01-16 06:17:26 +0100

    Added previously cut for the map method

--- wp-content/plugins/quiz-plugin/src/index.js
+++ wp-content/plugins/quiz-plugin/src/index.js
@@ -26,19 +26,23 @@ function EditComponent(props){
         label="Question:" value={props.attributes.question} onChange={updateQuestion} style={{fontSize:"20px"}}
       />
       <p style={{fontSize:"13px", margin:"20px 0 8px 0"}}>Answers:</p>
-      <Flex>
-        <FlexBlock>
-          <TextControl />
-        </FlexBlock>
-        <FlexItem>
-          <Button>
-            <Icon className='mark-as-correct' icon="star-empty"/>
-          </Button>
-        </FlexItem>
-        <FlexItem>
-          <Button variant='link' className='question-delete-btn'>Delete</Button>
-        </FlexItem>
-      </Flex>
+      {props.attributes.answers.map(function(answer){
+        return(
+          <Flex>
+            <FlexBlock>
+              <TextControl value={answer}/>
+            </FlexBlock>
+            <FlexItem>
+              <Button>
+                <Icon className='mark-as-correct' icon="star-empty"/>
+              </Button>
+            </FlexItem>
+            <FlexItem>
+              <Button variant='link' className='question-delete-btn'>Delete</Button>
+            </FlexItem>
+          </Flex>
+        )
+      })}
       <Button variant="primary">Add another answer</Button>
     </div>
   )
