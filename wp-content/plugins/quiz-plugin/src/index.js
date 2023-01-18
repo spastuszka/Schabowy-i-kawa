52dfde0d102026d3bceaba3042280dfd0e8dfc08
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2023-01-18 07:40:08 +0100

    Added logic about - bloking updat post when our quiz blokcs do not have checked correct answer

--- wp-content/plugins/quiz-plugin/src/index.js
+++ wp-content/plugins/quiz-plugin/src/index.js
@@ -1,5 +1,28 @@
 import './index.scss'
 import {TextControl, Flex, FlexBlock, FlexItem, Button, Icon} from '@wordpress/components'
+
+function ourStartFunction(){
+  let locked = false
+
+  wp.data.subscribe(function(){
+    const results = wp.data.select("core/block-editor").getBlocks().filter(function(block){
+      return block.name == "quiz-plugin/gutenberg-block-quiz" && block.attributes.correctAnswer == undefined
+    })
+
+    if(results.length && locked == false){
+      locked = true
+      wp.data.dispatch("core/editor").lockPostSaving("noanswer")
+    }
+
+    if(!results.length && locked){
+      locked = false
+      wp.data.dispatch("core/editor").unlockPostSaving("noanswer")
+    }
+  })
+}
+
+ourStartFunction();
+
 wp.blocks.registerBlockType('quiz-plugin/gutenberg-block-quiz',{
   title: "Quiz Block",
   icon: "smiley",
