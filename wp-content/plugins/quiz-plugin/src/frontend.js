9fc0ccb8e5f6007db9e36d3619377d2e5a04d786
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2023-01-22 09:15:01 +0100

    Added function to display forn-end

--- wp-content/plugins/quiz-plugin/src/frontend.js
+++ wp-content/plugins/quiz-plugin/src/frontend.js
@@ -3,5 +3,9 @@ import './frontend.scss'
 const divsToUpdate = document.querySelectorAll(".paying-attention-update-me")
 
 divsToUpdate.forEach(function(div){
-  div.innerHTML = "hello"
+  
 })
+
+function Quiz(){
+
+}
