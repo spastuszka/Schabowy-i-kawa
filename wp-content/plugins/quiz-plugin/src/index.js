9f9480a49eaf41b79be359182124fc5da548f101
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2023-03-02 08:03:32 +0100

    Modified quiz plugin like tested gutenberg plugin about uses recommended - block.json file

--- wp-content/plugins/quiz-plugin/src/index.js
+++ wp-content/plugins/quiz-plugin/src/index.js
@@ -1,6 +1,6 @@
 import './index.scss'
 import {TextControl, Flex, FlexBlock, FlexItem, Button, Icon, PanelBody, PanelRow, ColorPicker} from '@wordpress/components'
-import {InspectorControls, BlockControls, AlignmentToolbar} from '@wordpress/block-editor'
+import {InspectorControls, BlockControls, AlignmentToolbar, useBlockProps} from '@wordpress/block-editor'
 
 (function (){
   let locked = false
@@ -50,7 +50,12 @@ wp.blocks.registerBlockType('quiz-plugin/gutenberg-block-quiz',{
 })
 
 function EditComponent(props){
-    
+  
+  const blockProps = useBlockProps({
+    className:'paying-attention-edit-block',
+    style:{backgroundColor: props.attributes.bgColor}
+  });
+
   function updateQuestion(value){
     props.setAttributes({question:value})
   }
@@ -77,7 +82,7 @@ function EditComponent(props){
   }
 
   return(
-    <div className='paying-attention-edit-block' style={{backgroundColor: props.attributes.bgColor}}>
+    <div {...blockProps}>
       <BlockControls>
         <AlignmentToolbar value={props.attributes.theAlignment} onChange={x => props.setAttributes({theAlignment: x})}/>
       </BlockControls>
