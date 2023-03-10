79058d59db199da221bcfefb6d3f0b2aa4688d3f
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2023-03-10 07:00:31 +0100

    Created basic function to generates HTML to the user

--- wp-content/plugins/featured-cooker/inc/generateCookerHTML.php
+++ wp-content/plugins/featured-cooker/inc/generateCookerHTML.php
@@ -1,6 +1,20 @@
 <?php
-
+/* It is the function that generates the HTML to the user */
 function generateCookerHTML($id)
 {
-  return 'hello from new file';
+  $cookPost = new WP_Query(array(
+    'post_type' => 'cooker',
+    'p' => $id
+  ));
+
+  while ($cookPost->have_posts()) {
+    $cookPost->the_post();
+    ob_start(); ?>
+
+    <div class="cooker-callout">Test</div>
+
+<?php
+    wp_reset_postdata();
+    return ob_get_clean();
+  }
 }
