437a33dbace9cfe2304d3476925da6d22af9dcbd
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2023-03-17 08:00:05 +0100

    Added escape function to more secure data

--- wp-content/plugins/featured-cooker/inc/generateCookerHTML.php
+++ wp-content/plugins/featured-cooker/inc/generateCookerHTML.php
@@ -17,8 +17,8 @@ function generateCookerHTML($id)
       <div class="cooker-callout__photo" style="background-image: url(<?php the_post_thumbnail_url('cookerPortrait') ?>);"></div>
       <!-- This is a description about cooker -->
       <div class="cooker-callout__text">
-        <h5><?php the_title(); ?></h5>
-        <p><?php echo wp_trim_words(get_the_content(), 30) ?></p>
+        <h5><?php echo esc_html(get_the_title()); ?></h5>
+        <p><?php esc_html_e(wp_trim_words(get_the_content(), 30)); ?></p>
         <p><strong><a href="<?php the_permalink(); ?>">Learn more about <?php the_title(); ?> &raquo;</a></strong></p>
       </div>
     </div>
