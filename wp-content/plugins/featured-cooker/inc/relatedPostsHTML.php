071ed72f3020b79ff85c9f537af78a2f4bb49ead
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2023-03-22 07:17:21 +0100

    Created test return

---
+++ wp-content/plugins/featured-cooker/inc/relatedPostsHTML.php
@@ -0,0 +1,7 @@
+<?php
+
+/* To jest funkcja generująca html zwrotną powiązanych skróconych info w postach z wizytówką kucharza */
+function relatedPostsHTML($id)
+{
+  return "Hello 123 from relatesPostsHTML";
+}
