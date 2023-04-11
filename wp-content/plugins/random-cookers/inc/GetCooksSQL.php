f1ccaa6fa9abe96a7dcc51eefbfa9981efec6dc8
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2023-04-11 20:46:07 +0200

    Added getArgs function wichi spefiified url arguments

--- wp-content/plugins/random-cookers/inc/GetCooksSQL.php
+++ wp-content/plugins/random-cookers/inc/GetCooksSQL.php
@@ -10,28 +10,29 @@
       /* Utworzenie prefiksu dynamicznego dla customowej tabeli */
       $tablename = $wpdb->prefix . 'cooks';
 
+      $this->args = $this->getArgs();
+
       /* Podstawowe zahardkodowanie początku zapytania oraz połączenie całego zapytania dynamicznego z trzech części*/
       $query = "SELECT * FROM $tablename ";
       /* Tu będzie metoa przeszukująca odpowiednie kolumny */
       $query .= $this->createWhereText();
       $query .= " LIMIT 100";
 
-      $this->args = $this->getArgs();
-
       $this->cooks = $wpdb->get_results($wpdb->prepare($query, $this->args));
     }
-  }
 
-  /* Funkcja przekazaująca odpowiednie argumenty w zależności, czy dany warunek zgodzi się z przekazanymi kluczami w url */
-  function getArgs()
-  {
-    $temp = [];
+    /* Funkcja przekazaująca odpowiednie argumenty w zależności, czy dany warunek zgodzi się z przekazanymi kluczami w url */
+    function getArgs()
+    {
+      $temp = [];
 
-    if (isset($_GET['cookname'])) $temp['cookname'] = sanitize_text_field($_GET['cookname']);
-    if (isset($_GET['cookweight'])) $temp['cookweight'] = sanitize_text_field($_GET['cookweight']);
-    if (isset($_GET['favhobby'])) $temp['favhobby'] = sanitize_text_field($_GET['favhobby']);
-    if (isset($_GET['favfood'])) $temp['favfood'] = sanitize_text_field($_GET['favfood']);
-    if (isset($_GET['birthyear'])) $temp['birthyear'] = sanitize_text_field($_GET['favfood']);
+      if (isset($_GET['cookname'])) $temp['cookname'] = sanitize_text_field($_GET['cookname']);
+      if (isset($_GET['favhobby'])) $temp['favhobby'] = sanitize_text_field($_GET['favhobby']);
+      if (isset($_GET['favfood'])) $temp['favfood'] = sanitize_text_field($_GET['favfood']);
+      if (isset($_GET['minyear'])) $temp['minyear'] = sanitize_text_field($_GET['minyear']);
+      if (isset($_GET['maxyear'])) $temp['maxyear'] = sanitize_text_field($_GET['maxyear']);
+      if (isset($_GET['minweight'])) $temp['minweight'] = sanitize_text_field($_GET['minweight']);
+      if (isset($_GET['maxweight'])) $temp['maxweight'] = sanitize_text_field($_GET['maxweight']);
 
     return $temp;
   }
